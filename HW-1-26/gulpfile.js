af90d191eccdc669979c66d30e42a693da457028
Noah Wisch <noah.wisch@gmail.com>
2017-01-29 14:43:50 -0500

    Updated gulpfile and some small styling

--- HW-1-26/gulpfile.js
+++ HW-1-26/gulpfile.js
@@ -3,9 +3,10 @@
 // Things like converting Sass to CSS, removing comments, mergin files, etc.
 
 // 1. Import gulp
-let gulp = require('gulp');
-let sass = require('gulp-sass');
-let browser = require('gulp-browser');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browser = require('gulp-browser');
+const strip = require('gulp-strip-comments');
 
 // 2. create default tasks
 gulp.task('default', ['html', 'css', 'js']);
@@ -14,14 +15,16 @@ gulp.task('default', ['html', 'css', 'js']);
 gulp.task('html', function () {
     // Copy index.html into the public/ directory
     return gulp.src('index.html')
-        .pipe(gulp.dest('public/'));
+        .pipe(gulp.dest('public/'))
+        .pipe(strip());
 });
 
 gulp.task('css', function () {
     // Convert style.scss to style.css
-    // Copy css file to public/
+    // Copy css file to public/ssssssss
     return gulp.src('scss/style.scss')
         .pipe(sass()) // requires gulp-sass
+        .pipe(strip())
         .pipe(gulp.dest('public/'));
 });
 
@@ -29,10 +32,11 @@ gulp.task('js', function () {
     // Copy js file to public/
     return gulp.src('js/app.js')
         .pipe(browser.browserify())
+        .pipe(strip())
         .pipe(gulp.dest('public/'));
 });
 
-gulp.task('watch', ['default'], function() {
+gulp.task('watch', ['default'], function () {
     //gulp.watch('files-to-watch', 'task-to-run')
     gulp.watch('*.html', ['html']);
     gulp.watch('scss/*.scss', ['css']);
